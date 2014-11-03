d26e60ba1fdda9e4e258ad2fdaed77d4e76a72b4
magiccia <magiccia@gmail.com>
2014-11-02 19:26:57 -0600

    all the changes to publish my first website

--- tarbell_config.py
+++ tarbell_config.py
@@ -32,8 +32,8 @@ S3_BUCKETS = {
     # Provide target -> s3 url pairs, such as:
     #     "mytarget": "mys3url.bucket.url/some/path"
     # then use tarbell publish mytarget to publish to it
-    "production": "recoveredfactory.net",
-    "staging": "tarbell.recoveredfactory.net/eads-portfolio",
+    "production": "www.inforight.net",
+    "staging": "staging.inforight.net",
 }
 
 # Default template variables
